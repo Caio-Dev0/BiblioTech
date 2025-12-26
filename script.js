7d20a61492a9da546b31cafbf7a30b5762c8754f
Caio Lucas <clucaszrtmain@gmail.com>
2025-12-25 22:21:43 -0300

    refactor: Muda a filtragem de paginas de 500 para 300

--- script.js
+++ script.js
@@ -9,6 +9,6 @@ class Livro{
 
 let ArrLivros = [new Livro("Meu amigo carinhoso", 'Robero', 15), new Livro("Aprendendo Javascript", 'Robeto', 550), new Livro("CSS Eficiente", 'Robepo', 195), new Livro("O que é o Java?", 'Ropero', 2300)]
 
-let ArrLivros300 = ArrLivros.filter(a => a.paginas >= 500)
+let ArrLivros300 = ArrLivros.filter(a => a.paginas >= 300)
 
 console.log(ArrLivros300)
