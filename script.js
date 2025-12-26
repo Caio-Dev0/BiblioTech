c3646e69fd348a3a9ec6db72628acb179580fcfc
Caio Lucas <clucaszrtmain@gmail.com>
2025-12-25 22:19:32 -0300

    Adiciona o script do projeto resolvido

---
+++ script.js
@@ -0,0 +1,14 @@
+class Livro{
+    constructor(titulo, autor, paginas){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.paginas = paginas;
+    }
+}
+
+
+let ArrLivros = [new Livro("Meu amigo carinhoso", 'Robero', 15), new Livro("Aprendendo Javascript", 'Robeto', 550), new Livro("CSS Eficiente", 'Robepo', 195), new Livro("O que é o Java?", 'Ropero', 2300)]
+
+let ArrLivros300 = ArrLivros.filter(a => a.paginas >= 500)
+
+console.log(ArrLivros300)
